4d34ceed8616fda53569c0241b9f90f776be6671
Miguel <miguel251028@gmail.com>
2025-03-04 18:14:41 -0300

    Inicio do Projeto

---
+++ src/main/java/com/miguel/ProjetoSping/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package com.miguel.ProjetoSping.screenmatch;
+
+import com.miguel.ProjetoSping.screenmatch.model.DadosSerie;
+import com.miguel.ProjetoSping.screenmatch.service.ConsumoApi;
+import com.miguel.ProjetoSping.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=breaking+bad&apikey=1c42b734");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
